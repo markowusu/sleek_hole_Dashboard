d9b19ca692ede8800ccce7bca64a2746fcf6bde0
Mark Owusu <markowusuayim@gmail.com>
2023-07-19 18:45:25 +0100

    add: used the appropriate type for the onChange event on the input field

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -1,13 +1,13 @@
 import { ChangeEventHandler } from "react";
 
-function Input({type,value,onChange,description,cls,name,errorMessage}:{type:string,value:string,onChange?:ChangeEventHandler<HTMLInputElement>,description:string,cls?:string,name:string, errorMessage?: string}){
+function Input({type,value,onChange,onBlur,description,cls,name,errorMessage}:{type:string,value:string,onChange?:ChangeEventHandler<HTMLInputElement>,onBlur?:ChangeEventHandler<HTMLInputElement>,description:string,cls?:string,name:string, errorMessage?: string}){
    
     return(
         <>
         <label className="block mb-2 text-sm font-normal text-gray-700">
             {name}
         </label>
-        <input type={type} id="email" name="email" className={`w-full px-3 py-1 text-gray-700 border rounded-md ${cls}`} placeholder={description} value={value} onChange={onChange}/>
+        <input type={type} id="email" name="email" className={`w-full px-3 py-1 text-gray-700 border rounded-md ${cls}`} placeholder={description} value={value} onChange={onChange} onBlur={onBlur}/>
         <p className="my-2 font-sans text-base font-normal text-red-500 ">
             {errorMessage}
             </p>
