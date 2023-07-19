721f81ed67afbab9eca212b1b850687e970ab5d8
Mark Owusu <markowusuayim@gmail.com>
2023-07-19 18:01:49 +0100

    add: button role on the cardButton component to behave like a button

--- app/components/cardButton.tsx
+++ app/components/cardButton.tsx
@@ -1,6 +1,6 @@
 function CardButton({cls="", text}:{cls?:string, text:string}){
     return(
-        <div className={`mx-6 text-black border rounded-md  shadow-md h-[36px] border-gray-300  px-12 py-4  flex justify-center items-center align-middle hover:shadow-lg hover:bg-slate-100 ${cls}`}> 
+        <div className={`mx-6 text-black border rounded-md  shadow-md h-[36px] border-gray-300  px-12 py-4  flex justify-center items-center align-middle hover:shadow-lg hover:bg-slate-100 ${cls}`} role="button"> 
         <p className="text-center">{text}</p>
     </div>
     );
