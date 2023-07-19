06dc84c4ac9f58d9d268c2863dc761698530adda
Mark Owusu <markowusuayim@gmail.com>
2023-07-19 18:47:54 +0100

    add: changed the type on the onCLick event

--- app/components/cardButton.tsx
+++ app/components/cardButton.tsx
@@ -1,6 +1,6 @@
-function CardButton({cls="", text}:{cls?:string, text:string}){
+function CardButton({cls="", text,onClick }:{cls?:string, text:string, onClick?:any}){
     return(
-        <div className={`mx-6 text-black border rounded-md  shadow-md h-[36px] border-gray-300  px-12 py-4  flex justify-center items-center align-middle hover:shadow-lg hover:bg-slate-100 ${cls}`} role="button"> 
+        <div className={`mx-6 text-black border rounded-md  shadow-md h-[36px] border-gray-300  px-12 py-4  flex justify-center items-center align-middle hover:shadow-lg hover:bg-slate-100 ${cls}`} role="button" onClick={onClick}> 
         <p className="text-center">{text}</p>
     </div>
     );
